76b2f49fbb44e66a2fc23fe54b1f6c44c7c8f778
Moein Akbarof <moein.akbarof@typeform.com>
2020-08-19 09:15:11 +0200

    test

---
+++ precache-manifest.86fdcb7596af56e5d7ae3c8d94ff9d0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2e935c4052aaf272bc12",
+    "url": "/static/css/main.e5d6f455.chunk.css"
+  },
+  {
+    "revision": "2e935c4052aaf272bc12",
+    "url": "/static/js/main.2e935c40.chunk.js"
+  },
+  {
+    "revision": "24263722fe68298bce05",
+    "url": "/static/css/1.c5a3adbc.chunk.css"
+  },
+  {
+    "revision": "24263722fe68298bce05",
+    "url": "/static/js/1.24263722.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f517ff0554ba1839562c8e57eee46fda",
+    "url": "/index.html"
+  }
+];
